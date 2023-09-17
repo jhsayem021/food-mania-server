3a3297b18e229e30d42d407dc6795c1a8383444b
jhsayem021 <sayemmetro420@gmail.com>
2023-09-17 12:20:22 +0600

    vercel adjust

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const verifyJWT = (req, res, next) => {
 }
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sezawpu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sezawpu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
